606b0b9fdec47928e87620676a8eb87b4d783435
JKodes <adrainford09@gmail.com>
2024-03-13 03:15:44 -0400

    new template

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from movies.views import index
+from movies.views import about
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
+    path('about/', about)
 ]
--- movies/views.py
+++ movies/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello Welcome to my favroite movies")
+    context ={
+        'movies' : ['titanic', 'maximum overdrive', 'its a mad mad world']
+    }
+    return render(request, 'movies/index.html', { context })
+
+def about(request):
+    return render(request, 'movies/about.html', {})
