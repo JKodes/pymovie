73eb3f8a791a7597710f8bbf9c6db8579a1cf1b0
JKodes <adrainford09@gmail.com>
2024-03-15 23:49:23 -0400

    add a context array

--- movies/views.py
+++ movies/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def index(request):
-    context ={
+    context = {
         'movies' : ['titanic', 'maximum overdrive', 'its a mad mad world']
     }
-    return render(request, 'movies/index.html', { context })
+    return render(request, 'movies/index.html', context)
 
 def about(request):
     return render(request, 'movies/about.html', {})
